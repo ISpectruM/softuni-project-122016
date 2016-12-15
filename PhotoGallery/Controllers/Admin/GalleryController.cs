d0411cb3dae772eb2f137ce2425f4e5bccc4ea8c
ISpectruM <ispectrumcode@gmail.com>
2016-12-15 02:21:59 +0200

    Added: create and List functions to GalleryController and created Views.

---
+++ PhotoGallery/Controllers/Admin/GalleryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotoGallery.Models;
+
+namespace PhotoGallery.Controllers.Admin
+{
+    [Authorize(Roles = "Admin")]
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult List()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var galleries  = db.Galleries
+                    .Include(g=>g.Images)
+                    .OrderBy(g => g.Name)
+                    .ToList();
+                
+                return View(galleries);
+            }
+        }
+
+        //GET: Gallery/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Gallery/Create
+        [HttpPost]
+        public ActionResult CreateAction(Gallery gallery)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ApplicationDbContext())
+                {
+                    db.Galleries.Add(gallery);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("List");
+        }
+
+
+    }
+
+}
--- PhotoGallery/Controllers/ImageController.cs
+++ PhotoGallery/Controllers/ImageController.cs
@@ -21,5 +21,6 @@ namespace PhotoGallery.Controllers
             }
             return View();
         }
+
     }
 }
